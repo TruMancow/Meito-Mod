5cf8c73f142562753ea6ba9b99f8bbcd426598f0
TruMancow <81778227+TruMancow@users.noreply.github.com>
2021-04-01 17:01:48 -0400

    Add files via upload

---
+++ MeitoMod/Swords/Great Grade/AmeNoHabakiri/AmeNoHabakiri.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MeitoMod.Swords.GreatGrade.AmeNoHabakiri
+{
+	public class Enma : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Hmm... I wonder if you could talk to someone to figure out more about this sword...");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 250;
+			item.melee = true;
+			item.width = 80;
+			item.height = 80;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.knockBack = 6;
+			item.value = 54;
+			item.rare = ItemRarityID.Yellow;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
---
+++ MeitoMod/Swords/Great Grade/Enma/Enma.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MeitoMod.Swords.GreatGrade.Enma
+{
+	public class Enma : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Enma doesn't seem to respond well to your commands. Maybe if you could prove yourself to her...");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 250;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.knockBack = 6;
+			item.value = 54;
+			item.rare = ItemRarityID.Yellow;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
